00c8f0847e285ac87b19b222ab33121c08e70ce7
Sara Raii <124457589+sararaii@users.noreply.github.com>
2023-11-19 15:41:23 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,365 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" sizes="16px" type="jpg" href="140A8F6F-DB6D-4A70-9701-83E8AE685D71-removebg-.webp">
+    <link href="https://fonts.googleapis.com/css2?family=Cinzel+Decorative&family=Cinzel:wght@500&family=Cormorant+Garamond:ital,wght@1,300;1,600&family=EB+Garamond:ital@1&family=Playfair+Display&display=swap" rel="stylesheet">   <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@500&family=Cormorant+Garamond:ital,wght@1,300;1,600&family=Playfair+Display&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@500&family=Cormorant+Garamond:ital,wght@1,300;1,600&display=swap" rel="stylesheet"><link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@1,300;1,600&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Baskervville&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@500&family=Cormorant+Garamond:ital,wght@1,300;1,600&family=EB+Garamond:ital@1&family=Playfair+Display&display=swap" rel="stylesheet">
+   <link href="https://fonts.googleapis.com/css2?family=Cinzel+Decorative&display=swap" rel="stylesheet">
+   <link rel="icon" href="favicon.jpg" type="image/x-icon">
+   <link rel="stylesheet" href="popup.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+  <link href="https://icons8.com/icon/Nv2jcZScW9Pt/subscription">
+    <!-- Boxicons CSS -->
+<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'><link rel="shortcut icon" href="favicon.jpg" type="image/x-icon">
+  
+<title>Candela Shop</title>
+
+</head>
+<body>
+  
+    <main>
+      <section id="hero">
+          
+        <div class="promotion-bar">
+            <p>Limited Time Offer! Free Shipping on Orders Over $50. Shop Now!</p>
+        </div>
+        <div class="title" >
+            <div class="logo">
+                 <h1> Candela </h1>
+            </div>
+        <div class="navigation">
+            <nav>
+                <div  class="nav-buttons" id="nav-bar-home" style="display: inline-block;">
+                    <a  href="index.html">Home</a>
+                </div>
+                <div  class="nav-buttons" id="nav-bar-AboutUs" style="display: inline-block;">
+                    <a href="#our-story">About Us</a>
+                </div>
+                <div class="nav-buttons" id="nav-bar-ShopNow" style="display: inline-block;">
+                <a  href="care.html">Candle Care</a>
+                </div>
+                <div class="nav-buttons" id="nav-bar-ContactUs" style="display: inline-block;">
+                    <a href="ContactUs.html">Contact Us</a>
+                    </div>
+               <div class="nav-buttons" id="nav-bar-ContactUs" style="display: inline-block;">
+                 <a href="addtocart.html"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAACXBIWXMAAAsTAAALEwEAmpwYAAAA5ElEQVR4nO3WrwrCQBzA8W+waFVQo/gIJpPB4hCsgqBPYDHrA4gvIZh8g3UNaza7OLOCYeDfiXBBLN7N305Bv3Dtx3243WCDX20OhGqtgOYn4BA4AUUsN1H4wDbcULBnG04BAXABsrZxV526YxvuPr1sUmv2Ci7EBG90Tr2MAR7rwKMY4JYOXBFGr0BOB04AO0F4gUFTQXhoArcF4aoJnAHOAmgAJDHME4BdItQXgHtR4DywfwPdAmkiVlaP/GgAHtQPRikq+rvV1dfFBxyBOe38h/tbC8x9P+yoTe+b1QTm/mGlGw7LH6npIjrWAAAAAElFTkSuQmCC"></a>
+                 </div>
+            </nav>
+     
+        </div>
+       
+      </div>
+</section>
+          <!--Secondary Nav Bar-->
+<nav   class="secondary-nav">
+  <ul class="nav2-content">
+    <li class="nav-list">
+      <a onclick="nav2()"  class="link-item active">
+        <i class='bx bxs-home link-icon'></i>
+        <span class="link-text">Home</span>
+
+      </a>
+    </li>
+    <li class="nav-list">
+      <a onclick="nav2()" href="Shop.html" class="link-item ">
+        <i class='bx bxs-cart-alt link-icon'></i>
+        <span class="link-text">Shop</span>
+
+      </a>
+    </li>
+    <li class="nav-list">
+      <a onclick="nav2()" href="care.html" class="link-item ">
+        <i class='bx bxs-flame link-icon'></i>
+        <span class="link-text">Candle Care</span>
+
+      </a>
+    </li>
+    <li class="nav-list">
+      <a onclick="nav2()" href="ContactUs.html" class="link-item ">
+        <i class='bx bxs-phone link-icon' ></i>
+        <span class="link-text">Contact Us</span>
+
+      </a>
+    </li>
+    
+  </ul>
+</nav>
+
+<!-- Slideshow container -->
+<div class="slideshow-container">
+
+  <div class="mySlides fade"> 
+    <img src="Home.png" >
+    <div class="text">
+    <h1>Light it into existance</h1>
+      <a href="shop.html"><button class="button">Shop Now</button></a>
+      
+
+    </div>
+  </div>
+
+  <div class="mySlides fade">
+  <img src="Home (2).png" style="width:100%">
+  <div class="text">
+  <h1>Explore our candle care tips </h1>
+      <a href="candlecare.html"><button class="button">Candle care</button></a>
+      
+    </div>
+</div>
+
+<div class="mySlides fade">
+  <img src="Home (3).png" style="width:100%">
+  <div class="text">
+   <h1>Discover the artistry behind<br> our candles</h1>
+      <a href="aboutus.html"><button class="button">About Us</button></a>
+      
+    </div>
+</div>
+
+</div>
+
+
+<!-- The dots/circles -->
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span>
+  <span class="dot" onclick="currentSlide(2)"></span>
+  <span class="dot" onclick="currentSlide(3)"></span>
+</div>
+    
+
+
+      </section>
+
+       <section class="About">
+        <h1 id="about-title"> Discover Candela </h1>
+        <div id="first-statement">
+          <p>A combination of authentic fragrance and art.  The epitome of sophistication and luxury. </p>
+        </div>
+        <br>
+      <div id="second-statement">
+        <p>Home of the largest range of natural sculptural candles curated to enhance your space.</p>
+      </div>
+         <br>
+      <div id="third-statement">
+        <p >Each candle is individually handcrafted in Tyre, Lebanon with a natural soy wax and blended with the finest fragrances. </p>
+      </div>
+        <br>
+      </section>
+
+      <br>
+      
+ 
+ <div class="sections">
+  <div class="features">
+    <div class="feature">
+      <div id="candles">
+        <a href="Shop.html#Candle-section">
+          <h2 class="Shop-button" style="text-align: center; white-space: pre-wrap;">Shop Candles</h2>
+        </a>
+        <img src="ShopCandles.jpg" width="350px">
+      </div>
+    </div>
+    <div class="feature">
+      <div id="diffusers">
+        <a href="Shop.html#Diffuser-section">
+          <h2 class="Shop-button" style="text-align: center; white-space: pre-wrap;">Shop Diffusers</h2>
+        </a>
+        <img src="Shop-Diffuser.jpg" width="350px">
+      </div>
+    </div>
+    <div class="feature">
+      <div id="Accessories">
+        <a href="Shop.html#Accessories-section">
+          <h2 class="Shop-button" id="Shop-Accessories" style="text-align: center; white-space: pre-wrap;">Shop Accessories</h2>
+        </a>
+        <img src="Shop-Accessories.jpg" width="350px">
+      </div>
+    </div>
+  </div>
+</div>
+
+<script src="script.js"></script>
+    </main>
+    <br> 
+     
+  <input type="checkbox" id="toggle">
+  <label for="toggle" class="show-btn"><img width="48" height="48" src="https://img.icons8.com/color/48/subscription.png" alt="subscription"/></label>
+  <div class="wrapper">
+    <label for="toggle">
+    <i class="cancel-icon fas fa-times"></i>
+    </label>
+    <div class="icon"><i class="far fa-envelope"></i></div>
+    <div class="content">
+      <header>Candela Company</header>
+      <p>Sign up for exclusive offers and new releases</p>
+    </div>
+    <form action="index.php" method="POST">
+    <?php 
+    $userEmail = ""; //first we leave email field blank
+    if(isset($_POST['subscribe'])){ //if subscribe btn clicked
+      $userEmail = $_POST['email']; //getting user entered email
+      if(filter_var($userEmail, FILTER_VALIDATE_EMAIL)){ //validating user email
+        $subject = "Thanks for Subscribing us - Candelam Company";
+        $message = " You'll always receive updates from us. And we won't share and sell your information.";
+        $sender = "From: candelalightup@gmail.com";
+        //php function to send mail
+        if(mail($userEmail, $subject, $message, $sender)){
+          ?>
+           <!-- show sucess message once email send successfully -->
+          <div class="alert success-alert">
+            <?php echo "Thanks for Subscribing us." ?>
+          </div>
+          <?php
+          $userEmail = "";
+        }else{
+          ?>
+          <!-- show error message if somehow mail can't be sent -->
+          <div class="alert error-alert">
+          <?php echo "Failed while sending your mail!" ?>
+          </div>
+          <?php
+        }
+      }else{
+        ?>
+        <!-- show error message if user entered email is not valid -->
+        <div class="alert error-alert">
+          <?php echo "$userEmail is not a valid email address!" ?>
+        </div>
+        <?php
+      }
+    }
+    ?>
+      <div class="field">
+        <input type="text" class="email" name="email" placeholder="Email Address" required value="<?php echo $userEmail ?>">
+      </div>
+      <div class="field btn">
+        <div class="layer"></div>
+        <button type="submit" name="subscribe">Continue</button>
+      </div>
+    </form>
+    <div class="text"></div>
+  </div>
+
+<div id="our-story">
+  <div>
+    <h2 id="our-story-header" >Our Story</h2>
+  </div>
+  <div class="our-story-text">
+    <p>We are Candela Company. Our dreamy scents are inspired by nature and travel, moments and memories. We make our candles in our workshop using sustainable soy wax and phthalate-free perfume oils infused with essential oils. We pride ourselves on our meticulous craftsmanship, ethical production, and accessible prices.</p>
+  </div>
+</div>
+<br> <br>
+
+<div class="characteristics" style="display: grid; text-align: center;">
+ <img src="characteristics.png " width="850px" style="margin-right: 30px;">
+</div>
+
+<br><br>
+
+<video src="video.mp4" width="100%" controls>
+  This video isnt available.
+</video>
+
+
+<div class="container py-5">
+    <div class="row mb-4">
+      <div class="col-lg-5">
+        <h2 class="section-name">Meet Our Small Business Owners</h2>
+        <p class="our-story-text">Two enterprising teenagers have embarked on an entrepreneurial journey, successfully establishing a small candle business. With their passion for crafting unique candles and their entrepreneurial spirit, they have transformed their creative ideas into a thriving venture.</p>
+      </div>
+    </div>
+
+    <div class="row text-center">
+      
+      <div class="col-xl-3 col-sm-6 mb-5">
+        <div class="sections" id="hiba"><img src="hiba-pp.jpg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+          <h5 class="mb-0">Hiba Alawieh</h5><span class="small text-uppercase text-muted"> Founder</span>
+          <ul class="social mb-0 list-inline mt-3">
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+            <li class="list-inline-item"><a href="https://www.instagram.com/hiba_alawiehh/" class="social-link"><i class="fa fa-instagram"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+          </ul>
+        </div>
+      </div>
+      
+      <div class="col-xl-3 col-sm-6 mb-5">
+        <div class="sections " id="sara"><img src="sara-pp.jpg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+          <h5 class="mb-0">Sara Raii</h5><span class="small text-uppercase text-muted"> Founder</span>
+          <ul class="social mb-0 list-inline mt-3">
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+            <li class="list-inline-item"><a href="https://www.instagram.com/sara_raii/" class="social-link"><i class="fa fa-instagram"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+      
+    <div class="follow-us">
+      <h2 class="follow-us-header">Follow Us</h2>
+      <h3> @candela.companyy</h3>
+      <ul class="Footer-social-horizontalList">
+        <li class="horizontal-item">
+          <a href="https://www.facebook.com/candela light up/" style="display: in
+          ;">
+            <img src="20673.png" alt="facebook logo" height="20px" width="20px" >
+          </a>
+        </li>
+        <li class="horizontal-item">
+          <a href="https://www.instagram.com/candela.companyy/" style="display: inline;" >
+            <img src="instalogo.png" alt="facebook logo" height="20px" width="20px" >
+          </a>
+        </li>
+      </ul>
+    </div>
+    <footer>
+      
+     <div class="footer">
+        <label for="email" style="margin-left: 15px ;">Subscribe to get 10% off on your first order.</label>
+        <br>
+
+       
+    
+        <input type="email" id="email" name="email" placeholder="Enter your email address" maxlength="50">
+        
+        <a href="#"><button class="submit"  type="submit"  >Subscribe Now</button></a>
+      </div>
+
+      <div class="footer-items">
+          
+        <div class="border1" >
+
+          <ul style="
+          list-style: none;
+          display: flex;
+          flex-direction: row;
+          padding: 10px;
+          color: rgb(31, 31, 31);
+          font-size: 20px;
+           text-align: center;
+           justify-content: center;">
+           
+            <a href="index.html"><li style="padding: 10px;"><u>Home</u></li></a>
+            <a href="Shop.html"><li style="padding: 10px;"><u>Shop</u></li></a>
+            <a href="ContactUs.html"><li style="padding: 10px;"><u>Contact Us</u></li></a>
+            <a href="care.html"><li style="padding: 10px;"><u>Candle care</li></a>
+          </ul>
+      </div>
+        <p id="copyrights">&copy; 2023 Candela All rights reserved.</p>
+    </footer>
+    
+</body>
+
+</html>
